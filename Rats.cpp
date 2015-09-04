20e45fc3f48f16d47a148fc9c58731975b7caf10
TheSlugos <steve.powell36@gmail.com>
2015-09-04 22:09:46 +0930

    Split
    Created separate client and server functions and added console command
    processing to quit applications.

--- Rats.cpp
+++ Rats.cpp
@@ -1,23 +1,36 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
 #include <QuickDraw.h>
 #include <Timer.h>
 
 #include "Maze.h"
 #include "Actor.h"
 
-int main(int argc, char * argv [])
+using namespace std;
+
+// constants
+const int SERVERARGS = 2;	// program.exe <server-port>
+const int CLIENTARGS = 4;	// program.exe <client-port> <server-ip> <server-port>
+
+// Runs the server component of the application
+void server()
 {
+	cout << "Running as SERVER..." << endl;
+
 	QuickDraw window;
-	View & view = (View &) window;
-	Controller & controller = (Controller &) window;
+	View & view = (View &)window;
+	Controller & controller = (Controller &)window;
 
 	Maze model;
-	Player * player = new Player (controller);
-	model.addPlayer (player);
+	Player * player = new Player(controller);
+	model.addPlayer(player);
 
 	// Create a timer to measure the real time since the previous game cycle.
 	Timer timer;
-	timer.mark (); // zero the timer.
-	double lasttime = timer.interval ();
+	timer.mark(); // zero the timer.
+	double lasttime = timer.interval();
 	double avgdeltat = 0.0;
 
 	double scale = 1.0;
@@ -25,7 +38,7 @@ int main(int argc, char * argv [])
 	while (true)
 	{
 		// Calculate the time since the last iteration.
-		double currtime = timer.interval ();
+		double currtime = timer.interval();
 		double deltat = currtime - lasttime;
 
 		// Run a smoothing step on the time change, to overcome some of the
@@ -35,15 +48,111 @@ int main(int argc, char * argv [])
 		lasttime = lasttime + deltat;
 
 		// Allow the environment to update.
-		model.update (deltat);
+		model.update(deltat);
 
 		// Schedule a screen update event.
-		view.clearScreen ();
-    	double offsetx = 0.0;
-	    double offsety = 0.0;
-		(*player).getPosition (offsetx, offsety);
-		model.display (view, offsetx, offsety, scale);
-		view.swapBuffer ();
+		view.clearScreen();
+		double offsetx = 0.0;
+		double offsety = 0.0;
+		(*player).getPosition(offsetx, offsety);
+		model.display(view, offsetx, offsety, scale);
+		view.swapBuffer();
+
+		// console commands
+		if (_kbhit())
+		{
+			string input;
+
+			cout << "> ";
+
+			cin >> input;
+
+			if (input.find("/quit") != string::npos)
+			{
+				// quit the application
+				break;
+			}
+		}
+	}
+
+	// TODO: cleanup sockets, etc
+
+} // end server
+
+void client()
+{
+	cout << "Running as CLIENT..." << endl;
+
+	QuickDraw window;
+	View & view = (View &)window;
+	Controller & controller = (Controller &)window;
+
+	Maze model;
+	Player * player = new Player(controller);
+	model.addPlayer(player);
+
+	// Create a timer to measure the real time since the previous game cycle.
+	Timer timer;
+	timer.mark(); // zero the timer.
+	double lasttime = timer.interval();
+	double avgdeltat = 0.0;
+
+	double scale = 1.0;
+
+	while (true)
+	{
+		// Calculate the time since the last iteration.
+		double currtime = timer.interval();
+		double deltat = currtime - lasttime;
+
+		// Run a smoothing step on the time change, to overcome some of the
+		// limitations with timer accuracy.
+		avgdeltat = 0.2 * deltat + 0.8 * avgdeltat;
+		deltat = avgdeltat;
+		lasttime = lasttime + deltat;
+
+		// Allow the environment to update.
+		model.update(deltat);
+
+		// Schedule a screen update event.
+		view.clearScreen();
+		double offsetx = 0.0;
+		double offsety = 0.0;
+		(*player).getPosition(offsetx, offsety);
+		model.display(view, offsetx, offsety, scale);
+		view.swapBuffer();
+
+		// console commands
+		if (_kbhit())
+		{
+			string input;
+
+			cout << "> ";
+
+			cin >> input;
+
+			if (input.find("/quit") != string::npos)
+			{
+				// quit the application
+				break;
+			}
+		}
+	} // end while
+
+	// TODO: cleanup sockets, etc
+} // end client
+
+int main(int argc, char * argv [])
+{
+	if (argc == SERVERARGS)
+	{
+		// run server
+		server();
+	}
+	else if (argc == CLIENTARGS)
+	{
+		// run client
+		client();
 	}
 
 	return 0;
